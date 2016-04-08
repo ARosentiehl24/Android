c0209c7886204f60948ec42ffd4d5a628110c70c
Alberto R <alberto93-14@hotmail.com>
2016-04-08 06:31:42 -0500

    Added complete layout interface to calculator activity.

--- app/src/main/java/com/arrg/android/app/android/SplashScreenActivity.java
+++ app/src/main/java/com/arrg/android/app/android/SplashScreenActivity.java
@@ -3,17 +3,9 @@ package com.arrg.android.app.android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -40,14 +32,13 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onResume();
 
         if (finish) {
-            Toast.makeText(this, "The app will close automatically.", Toast.LENGTH_SHORT).show();
-
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                     finish();
                 }
-            }, 1250);
+            }, 500);
         }
     }
 
